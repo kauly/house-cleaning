7506b6a5d6dd40f3317c4ece18c342e04637a54a
kauly <kauly@hotmail.com>
2024-01-24 00:34:21 -0300

    fix: sql commented

--- app/page.tsx
+++ app/page.tsx
@@ -15,21 +15,21 @@ export default async function IndexPage({
 }: {
   searchParams: { q: string };
 }) {
-  const search = searchParams.q ?? '';
+  /*   const search = searchParams.q ?? '';
   const result = await sql`
     SELECT id, name, username, email 
     FROM users 
     WHERE name ILIKE ${'%' + search + '%'};
   `;
-  const users = result.rows as User[];
+  const users = result.rows as User[]; */
 
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <Title>Users</Title>
+      <Title>Clientes</Title>
       <Text>A list of users retrieved from a Postgres database.</Text>
       <Search />
       <Card className="mt-6">
-        <UsersTable users={users} />
+        <UsersTable users={[]} />
       </Card>
     </main>
   );
